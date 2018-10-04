8727da4056251c5bc01d02f7a6004fc8a2cc915a
gustavo-b <gustavo.hsbatista@gmail.com>
2018-10-03 22:36:24 -0300

    Criando mecanismo responsável por atulizar estado do jogo

---
+++ unity_project/Assets/Scripts/Updater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Updater : MonoBehaviour
+{
+    
+    [SerializeField] protected List<Material> materials;
+
+    private int currentSong;
+
+    // Use this for initialization
+    void Start()
+    {
+        Assert.IsTrue(materials.Count == 1 * Listener.possibleSongTypes);
+
+        AssignTexture();
+
+        currentSong = Listener.currentSong;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentSong != Listener.currentSong)
+        {
+            AssignTexture();
+            currentSong = Listener.currentSong;
+        }
+    }
+
+    protected void AssignTexture()
+    {
+        GetComponent<Renderer>().material = materials[Listener.currentSong];
+    }
+}
