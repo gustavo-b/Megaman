194f0931ad1bbaa8ade486284cb6312bd2299ace
gustavo-b <gustavo.hsbatista@gmail.com>
2018-12-06 23:34:39 -0200

    Confirmação de autorização

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ http.createServer(function (req, res) {
 	fs.writeFile (dir + "\\auth.txt", req.url, function(err) {
 		if (err) throw err;
 		console.log('complete');
-	}
-);
+		fs.readFile('./index.html', function (ex, html) {
+			if (ex) {
+				throw ex; 
+			}
+			res.writeHeader(200, {"Content-Type": "text/html"});  
+			res.write(html);
+			res.end();  
+		});
+	});
 }).listen(8080); //the server object listens on port 8080
