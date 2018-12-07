7c786a7f173bb78c3aa9bad48e4bfd7ade28b9e7
gustavo-b <gustavo.hsbatista@gmail.com>
2018-12-06 23:51:38 -0200

    Servidor

--- server.js
+++ server.js
@@ -8,17 +8,10 @@ http.createServer(function (req, res) {
 	if (!fs.existsSync(dir)){
 		fs.mkdirSync(dir);
 	}
-	
 	fs.writeFile (dir + "\\auth.txt", req.url, function(err) {
 		if (err) throw err;
 		console.log('complete');
-		fs.readFile('./index.html', function (ex, html) {
-			if (ex) {
-				throw ex; 
-			}
-			res.writeHeader(200, {"Content-Type": "text/html"});  
-			res.write(html);
-			res.end();  
-		});
 	});
+	
+	
 }).listen(8080); //the server object listens on port 8080
